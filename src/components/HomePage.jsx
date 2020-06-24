9ee0e2b67b7656f4807cca6ab27da452629a4ec7
ElieudoMaia <elieudo.maia@gmail.com>
2020-06-24 12:58:35 -0300

    Edit home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 
 export default () => {
     return (
-        <div className="row">
-            <h1>Home page for this app</h1>
+        <div className="row mt-3">
+
+            <div className="col">
+                <h1>Home Page</h1>
+
+                <h2>What is this application about?</h2>
+                
+                <p>This application consumes an API and implements a simple CRUD between two entities (Course and Student).</p>
+                
+                <p>
+                    To access the repository for this application <a target="_blank" rel="noopener noreferrer" href="https://github.com/ElieudoMaia/college-node-api">click here.</a>
+                </p>
+                <p>
+                To access the API repository <a target="_blank" rel="noopener noreferrer" href="https://github.com/ElieudoMaia/college-node-api">click here.</a>
+                </p>
+
+            </div>
         </div>
+            
     )
 }
